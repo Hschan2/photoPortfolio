021e11a84538eca921ece5c88eb62b8a0775c28f
Hchan2 <39434913+Hschan2@users.noreply.github.com>
2022-04-07 20:10:50 +0900

    Gallery 1차 개발

---
+++ index.js
@@ -0,0 +1,8 @@
+var userFeed = new Instafeed({
+    get: 'user',
+    target: "instafeeds",
+    resolution: 'high_resolution',
+    accessToken: 'IGQVJXNThJTmlzV2dUU3IyUmdqUWh4X0VRR3NDV01JN2Fxc3VVdDlqVERFcE5ORlozdW41Vk92NHpsX2RoOFFCZAUxLbFJTdWlKaE1QRGhnblloLUpjSlhGeURIbjdIYzZAIbnBpdHJoWm9DcVhNYzFDVQZDZD'
+});
+
+userFeed.run();
